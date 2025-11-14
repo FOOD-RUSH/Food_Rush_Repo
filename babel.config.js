0127ce4efbf2276b71dd8f5a15c61e548e39cbe1
MrCalculusPro <tochukwupaul21@gmail.com>
2025-11-15 00:39:05 +0100

    chore: update babel configuration for improved JSX handling

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
 
-  const plugins = [];
-
-  // Remove console.log, console.debug, console.info in production
-  // Keep console.error and console.warn for error tracking
-  if (process.env.NODE_ENV === 'production') {
-    plugins.push([
-      'transform-remove-console',
-      {
-        exclude: ['error', 'warn'],
-      },
-    ]);
-  }
+  
 
   return {
     presets: [
@@ -25,6 +14,5 @@ module.exports = function (api) {
       ],
       'nativewind/babel',
     ],
-    plugins,
   };
 };
